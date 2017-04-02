40d3151bc39eb42beeff52ef92ef9401e19eb9a6
Alex Wibowo <alexwibowo@gmail.com>
2017-04-02 22:26:18 +1000

    Added method to extract data from the sheet.

--- src/main/java/org/isolution/excel2bean/SheetUtil.java
+++ src/main/java/org/isolution/excel2bean/SheetUtil.java
@@ -7,26 +7,51 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Set;
-import java.util.stream.StreamSupport;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 
-import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
 
 public final class SheetUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(SheetUtil.class);
 
     /**
-     * @param sheet sheet to read from
-     * @return all field names in a given sheet
+     * @param sheet sheet to read field names from
+     * @return all field names in a given sheet, in the order the columns are defined
      */
     @NotNull
-    public static Set<String> readFieldNames(final @NotNull Sheet sheet) {
-        LOGGER.info("Reading field names for {}", sheet.getSheetName());
+    public static LinkedHashSet<String> readFieldNames(final @NotNull Sheet sheet) {
+        LOGGER.info("Reading field names from {}", sheet.getSheetName());
         final Row headerRow = sheet.getRow(0);
-        final Set<String> fieldNames = StreamSupport.stream(headerRow.spliterator(), false)
+        final LinkedHashSet<String> fieldNames = stream(headerRow.spliterator(), false)
                 .map(Cell::getStringCellValue)
-                .collect(toSet());
+                .collect(toCollection(LinkedHashSet::new));
         LOGGER.debug("Field names for {} are: {}", sheet.getSheetName(), fieldNames);
         return fieldNames;
+    }
+
+    /**
+     * @param sheet sheet to read data from
+     * @return List of data
+     */
+    @NotNull
+    public static List<Map<String, String>> readData(final @NotNull LinkedHashSet<String> fieldNames,
+                                                     final @NotNull Sheet sheet) {
+        LOGGER.info("Reading data from {}, expecting field {}", sheet.getSheetName(), fieldNames);
+        return stream(sheet.spliterator(), false)
+                .skip(1) // first row in sheet is the field names
+                .map(worksheetRow -> {
+                    final Map<String, String> row = new HashMap<>();
+                    stream(fieldNames.spliterator(), false)
+                            .forEach(fieldName -> {
+                                final Cell column = worksheetRow.getCell(row.size(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                                row.put(fieldName, column.getStringCellValue());
+                            });
+                    return row;
+                }).collect(toList());
     }
 }
--- src/test/java/org/isolution/excel2bean/SheetUtilTest.java
+++ src/test/java/org/isolution/excel2bean/SheetUtilTest.java
@@ -1,33 +1,74 @@
 package org.isolution.excel2bean;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static java.util.stream.StreamSupport.stream;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+@SuppressWarnings("unchecked")
 public class SheetUtilTest {
+
+    private XSSFWorkbook workbook;
+
+    @Before
+    public void setup() {
+        workbook = new XSSFWorkbook();
+        final XSSFSheet sheet = workbook.createSheet();
+        addRow(sheet, new String[]{"StudentId", "Firstname", "Surname"});
+        addRow(sheet, new String[]{"1", "Alex", "Wibowo"});
+        addRow(sheet, new String[]{"2", "Randy", "Wong"});
+    }
+
+    private XSSFRow addRow(final XSSFSheet sheet,
+                           final String[] values) {
+        final long rowCount = stream(sheet.spliterator(), false).count();
+        final XSSFRow newRow = sheet.createRow( (int) rowCount);
+        for (int i = 0; i < values.length; i++) {
+            newRow.createCell(i).setCellValue(values[i]);
+        }
+        return newRow;
+    }
 
     @Test
     public void retrieves_object_field_names_from_first_row_in_sheet() {
-        final XSSFWorkbook workbook = new XSSFWorkbook();
-        final XSSFSheet sheet = workbook.createSheet();
-        final XSSFRow row1 = sheet.createRow(0);
-        row1.createCell(0).setCellValue("StudentId");
-        row1.createCell(1).setCellValue("Firstname");
-        row1.createCell(2).setCellValue("Surname");
+        final LinkedHashSet<String> fieldNames = SheetUtil.readFieldNames(workbook.getSheetAt(0));
+        assertThat(fieldNames, contains("StudentId", "Firstname", "Surname"));
+    }
 
-        final XSSFRow row2 = sheet.createRow(1);
-        row2.createCell(0).setCellValue(1);
-        row2.createCell(1).setCellValue("Alex");
-        row2.createCell(2).setCellValue("Wibowo");
+    @Test
+    public void retrieves_data_with_all_the_fields() {
+        final LinkedHashSet<String> fieldNames = new LinkedHashSet<>();
+        fieldNames.add("StudentId");
+        fieldNames.add("Firstname");
+        fieldNames.add("Surname");
 
-        final Set<String> fieldNames = SheetUtil.readFieldNames(sheet);
-        assertThat(fieldNames, containsInAnyOrder("StudentId", "Firstname", "Surname"));
+        final List<Map<String, String>> values = SheetUtil.readData(fieldNames, workbook.getSheetAt(0));
+        assertThat(values.size(), equalTo(2));
+
+        final Map<String, String> data1 = ImmutableMap.<String, String>builder()
+                .put("StudentId", "1")
+                .put("Firstname", "Alex")
+                .put("Surname", "Wibowo")
+                .build();
+
+        final Map<String, String> data2 = ImmutableMap.<String, String>builder()
+                .put("StudentId", "2")
+                .put("Firstname", "Randy")
+                .put("Surname", "Wong")
+                .build();
+
+        assertThat(values, contains(data1, data2));
     }
 
 }
